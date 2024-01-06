2fa300c40b9a10c6053f31594820ce9b583a1c43
Moarbue <83272310+Moarbue@users.noreply.github.com>
2024-01-07 00:32:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,41 @@
 #!/bin/sh
 
-set -xe
+set -e
 
-mkdir -p bin
+CC="gcc"
+CFLAGS="-Wall -Wextra"
+CFLAGS_DEBUG="-ggdb3 -g -O0"
+CFLAGS_RELEASE="-O3"
 
-gcc -ggdb -Wall -Wextra src/main.c src/tokenizer.c src/stack.c src/queue.c src/parser.c src/error.c src/operator.c -o bin/selectron -lm
+LIBS="-lm"
+SRC="src/*.c"
+BIN_PATH="bin"
+BIN="${BIN_PATH}/selectron"
+
+compile () {
+    set -x
+    mkdir -p $BIN_PATH
+    $CC $CFLAGS -o $BIN $SRC $LIBS
+}
+
+if [ $# -eq 0 ]; then
+    CFLAGS="${CFLAGS} ${CFLAGS_RELEASE}"
+    compile
+fi
+
+for arg in "$@"
+do
+    case $arg in
+        debug)
+            CFLAGS="${CFLAGS} ${CFLAGS_DEBUG}"
+            compile
+            ;;
+        release)
+            CFLAGS="${CFLAGS} ${CFLAGS_RELEASE}"
+            compile 
+            ;;
+        clean)
+            ( set -x; rm -rf ${BIN_PATH:?}/* )
+            ;;
+    esac
+done
