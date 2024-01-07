2339e7024650fbfba937fceb91eb1c92bf03e3b2
Moarbue <83272310+Moarbue@users.noreply.github.com>
2024-01-07 16:20:42 +0100

    Add strings of error codes

--- src/error.c
+++ src/error.c
@@ -4,6 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char *ERROR_CODE_STRING[] = {
+    [ERROR_NO_ERROR]                = "ERROR_NO_ERROR",
+    [ERROR_NO_MEMORY]               = "ERROR_NO_MEMORY",
+    [ERROR_EXPRESSION_EMTPY]        = "ERROR_EXPRESSION_EMTPY",
+    [ERROR_EXCESS_DECIMAL_DELIM]    = "ERROR_EXCESS_DECIMAL_DELIM",
+    [ERROR_UNKNOWN_CHARACTER]       = "ERROR_UNKNOWN_CHARACTER",
+    [ERROR_UNKNOWN_EXPRESSION]      = "ERROR_UNKNOWN_EXPRESSION",
+    [ERROR_STACK_EMPTY]             = "ERROR_STACK_EMPTY",
+    [ERROR_QUEUE_EMPTY]             = "ERROR_QUEUE_EMPTY",
+    [ERROR_MISMATCHED_PARENTHESES]  = "ERROR_MISMATCHED_PARENTHESES",
+    [ERROR_LEADING_OPERATOR]        = "ERROR_LEADING_OPERATOR",
+    [ERROR_CALCULATION]             = "ERROR_CALCULATION",
+};
+
 error_t error(int code, const char fmt[], ...)
 {
     error_t e;
@@ -25,6 +39,6 @@ void log_on_error(error_t e)
 {
     if (e.code == ERROR_NO_ERROR) return;
 
-    printf("ERROR %d: %s\n", e.code, e.msg);
+    printf("%s: %s\n", ERROR_CODE_STRING[e.code], e.msg);
     exit(e.code);
 }
